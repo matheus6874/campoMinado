b04358a486c5a9521fc6687d9afd16fc9bd6bee4
matheus6874 <matheus.scristino@gmail.com>
2025-11-16 15:34:14 -0300

    Criacao dos metodos da classe campo e adicao dos testes unitarios

---
+++ campo-minado/src/main/java/org/matheus/cm/excecao/ExplosaoException.java
@@ -0,0 +1,5 @@
+package org.matheus.cm.excecao;
+
+public class ExplosaoException extends RuntimeException{
+
+}
--- campo-minado/src/main/java/org/matheus/cm/modelo/Campo.java
+++ campo-minado/src/main/java/org/matheus/cm/modelo/Campo.java
@@ -1,5 +1,7 @@
 package org.matheus.cm.modelo;
 
+import org.matheus.cm.excecao.ExplosaoException;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,15 +14,73 @@ public class Campo {
     private boolean marcado = false;
 
 
-    private List<Campo> vizinhpos = new ArrayList<>();
+    private List<Campo> vizinhos = new ArrayList<>();
 
-    Campo(int linha, int coluna){
+    public Campo(int linha, int coluna){
         this.linha = linha;
         this.coluna = coluna;
     }
 
-    //TODO
-    boolean adicionarVizinho(Campo vizinho){
-        return true;
+
+    public boolean adicionarVizinho(Campo vizinho){
+        boolean linhaDiferente = linha != vizinho.linha;
+        boolean colunDiferente = coluna != vizinho.coluna;
+        boolean diagonal = linhaDiferente && colunDiferente;
+
+        int deltaLinha = Math.abs(this.linha  - vizinho.linha);
+        int deltaColuna = Math.abs(this.coluna  - vizinho.coluna);
+        int deltaGeral = deltaColuna + deltaLinha;
+
+        if (deltaGeral == 1 && !diagonal){
+            vizinhos.add(vizinho);
+            return true;
+        }else if (deltaGeral == 2 && diagonal){
+            vizinhos.add(vizinho);
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public void alternarMarcacao(){
+        if (!aberto){
+            marcado = !marcado;
+        }
+    }
+
+    public boolean abrir(){
+        if (!aberto && !marcado){
+            aberto = true;
+            if (minado){
+                throw new ExplosaoException();
+            }
+            if (vizinhancaSegura()){
+                vizinhos.forEach(v -> v.abrir());
+            }
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+
+    boolean vizinhancaSegura(){
+        return vizinhos.stream().noneMatch(v -> v.minado);
+    }
+
+    public boolean isMarcado(){
+        return marcado;
+    }
+
+    public void minar(){
+        minado = true;
+    }
+
+    public boolean isAberto(){
+        return aberto;
+    }
+
+    public boolean isFechado(){
+        return !isAberto();
     }
 }
