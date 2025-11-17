6a1dc31785ebf0ce1d8b9c0c5983d31076074169
matheus6874 <matheus.scristino@gmail.com>
2025-11-17 10:35:41 -0300

    Implementacao do tabuleiro

--- campo-minado/src/main/java/org/matheus/cm/modelo/Tabuleiro.java
+++ campo-minado/src/main/java/org/matheus/cm/modelo/Tabuleiro.java
@@ -18,8 +18,22 @@ public class Tabuleiro {
         gerarCampos();
         associarVizinhos();
         sortearMinas();
-
     }
+
+    public void abrir(int linha, int coluna){
+        campos.parallelStream()
+                .filter(c -> c.getLinha() == linha && c.getColuna() == coluna)
+                .findFirst()
+                .ifPresent(c -> c.abrir());
+    }
+
+    public void alternarMarcacao(int linha, int coluna){
+        campos.parallelStream()
+                .filter(c -> c.getLinha() == linha && c.getColuna() == coluna)
+                .findFirst()
+                .ifPresent(c -> c.alternarMarcacao());
+    }
+
 
     private void gerarCampos() {
         for (int l=0;l<linhas;l++){
@@ -54,5 +68,21 @@ public class Tabuleiro {
     public void reiniciar(){
         campos.stream().forEach(c -> c.reiniciar());
         sortearMinas();
+    }
+
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        for (int l = 0; l < linhas; l++){
+            for (int c = 0; c < colunas; c++){
+                sb.append(" ");
+                sb.append(campos.get(i));
+                sb.append(" ");
+                i++;
+            }
+            sb.append("\n");
+        }
+
+        return sb.toString();
     }
 }
